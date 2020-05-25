12d0ba4a47278fdc46b0a8de4467984afc1f7811
Dilyen <davidandohacquah@gmail.com>
2020-05-25 16:12:03 -0400

    added TimerInterval

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -26,10 +26,9 @@ export class DashboardComponent implements OnInit {
 getData(){
     this.appservice.getStatusByPreviousDate().subscribe(response => {
         this.retrieved_previousDate = response
-        setInterval(() => {this.getData()}, 3000)
     });
 
-   
+    setInterval(() => {this.getData()}, 3000)
 
 }
 
@@ -41,9 +40,9 @@ getData(){
                 this.appservice.getStatusByCurrentDate().subscribe(response => {
                     this.retrieved_currentDate = response
                 });
-                //  this.appservice.getStatusByPreviousDate().subscribe(response => {
-                //     this.retrieved_previousDate = response
-                // });
+                 this.appservice.getStatusByPreviousDate().subscribe(response => {
+                    this.retrieved_previousDate = response
+                });
                 this.appservice.sendGetRequest().subscribe((resposeBody) => {
                     console.log(resposeBody);
                 });
