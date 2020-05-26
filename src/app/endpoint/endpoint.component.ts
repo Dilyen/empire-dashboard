e55537aa214aac71efd7968987e0cd31608a25f6
Dilyen <davidandohacquah@gmail.com>
2020-05-25 22:33:11 -0400

    made rout work

--- src/app/endpoint/endpoint.component.ts
+++ src/app/endpoint/endpoint.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestInput } from '../endpoints';
 
 @Component({
   selector: 'app-endpoint',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./endpoint.component.scss']
 })
 export class EndpointComponent implements OnInit {
+  public endpoints: any[] = [{ endpoints: '' }];
+
+public requestMethods: any[] = [{ requestMethods: '' }];
+
+request: RequestInput
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+
+  addRequestMethod() {
+    this.requestMethods.push({requestMethods: ''});
+}
+addEndpoint() {
+    this.request.urls.push({endpoint_url: name});
+}
+
 
 }
