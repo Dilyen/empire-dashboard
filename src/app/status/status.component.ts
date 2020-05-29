a5a9a6cb8d9c4d40eb82d5208894967070e4a238
Dilyen <davidandohacquah@gmail.com>
2020-05-29 13:13:53 -0400

    made to read endpoint_id

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmpireService } from '../empire.service';
-import { Status } from '../endpoints';
+import { Status, Endpoints } from '../endpoints';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -10,12 +10,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class StatusComponent implements OnInit {
-  retrieved_data: Status[] = [
-  
-
-]; 
+  retrieved_data: Status[] = []; 
+   
 
 project_id:string;
+endpoint_id: any
 
   constructor(private appservice: EmpireService, private route: ActivatedRoute) { }
 
@@ -24,9 +23,11 @@ project_id:string;
   
     this.route.paramMap.subscribe(params => {
       this.project_id = params.get("project_id")
+      this.endpoint_id= params.get("endpoint_id")
     })
 
       this.load_project_status(this.project_id)
+      this.load_project_status(this.endpoint_id)
 
   }
 
@@ -34,6 +35,9 @@ project_id:string;
     this.appservice.getStatusByProjectId(project_id)
     .subscribe(response=>{
       this.retrieved_data = response
+      this.appservice.addEndpoints(this.endpoint_id).subscribe(response =>{
+        this.endpoint_id= response
+      });
       
       // console.log("Response oooooooo ",response);
       })
